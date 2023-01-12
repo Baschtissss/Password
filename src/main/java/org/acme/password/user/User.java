b0f1dbb93b3337c9ac601a1ad8d3a949d1404400
Basti <sebastian2005witzeneder@gmail.com>
2023-01-12 14:20:38 +0100

    Initial User

---
+++ src/main/java/org/acme/password/user/User.java
@@ -0,0 +1,19 @@
+package org.acme.password.user;
+
+
+import lombok.*;
+
+import javax.persistence.Entity;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
+
+
+
+@Entity
+public class User {
+}
