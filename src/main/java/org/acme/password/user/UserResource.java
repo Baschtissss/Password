33a75159084286f11b4d038a2534ba24f517339f
Basti <sebastian2005witzeneder@gmail.com>
2023-01-12 15:02:47 +0100

    Added first test

--- src/main/java/org/acme/password/user/UserResource.java
+++ src/main/java/org/acme/password/user/UserResource.java
@@ -1,9 +1,25 @@
 package org.acme.password.user;
 
-import javax.inject.Inject;
+import org.acme.unicornrides.passenger.Passenger;
 
+import javax.inject.Inject;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+import java.net.URI;
+
+@Path("api/user")
 public class UserResource {
 
     @Inject
     UserRepository userRepository;
+
+    @POST
+    public Response register(final User user) {
+        if (user == null) {
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        }
+        final User createdUser = userRepository.registerUser(user);
+        return Response.ok(createdUser).build();
+    }
 }
